be0a42d6f72baf5bb5c7d150fa7c2986e8585d61
notminerproduction <64098611+notminerproduction@users.noreply.github.com>
2020-05-16 16:36:33 +0300

    Update index.js

--- app/controllers/account/index.js
+++ app/controllers/account/index.js
@@ -1,13 +1,7 @@
-import Controller from '@ember/controller';
-import { inject } from '@ember/controller';
-import { computed } from '@ember/object';
-import $ from 'jquery';
+import Ember from 'ember';
 import config from '../../config/environment';
 
-export default Controller.extend({
-  applicationController: inject('application'),
-  stats: computed.reads('applicationController'),
-  config: computed.reads('applicationController.config'),
+export default Ember.Controller.extend({  
   get config() {
         return config.APP;
     }
