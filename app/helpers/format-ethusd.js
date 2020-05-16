4819853680e1ab13980a1fd6c94b5cdb3bddf3f7
notminerproduction <64098611+notminerproduction@users.noreply.github.com>
2020-05-16 16:43:00 +0300

    Update format-ethusd.js

--- app/helpers/format-ethusd.js
+++ app/helpers/format-ethusd.js
@@ -1,4 +1,4 @@
-import Ember from 'ember';
+import { helper as buildHelper } from '@ember/component/helper';
 
 export function formatEthUsd(params) {
 	let value = params[0];       
@@ -13,5 +13,4 @@ export function formatEthUsd(params) {
 	return "US$" + inr.toFixed(2);
 }
 
-export default Ember.Helper.helper(formatEthUsd);
-
+export default buildHelper(formatEthUsd);
